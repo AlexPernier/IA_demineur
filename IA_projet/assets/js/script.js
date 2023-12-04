d5c63a06d2d005170ca88fa70aafc4c4c265d917
AlexPernier <alexandre.pernier@gmail.com>
2023-12-04 16:04:34 +0100

    add ia bfs

--- IA_projet/assets/js/script.js
+++ IA_projet/assets/js/script.js
@@ -9,24 +9,20 @@ let matrice_drapeaux;
 
 /// CHOIX DIFFICULTE ///
 
-function choix_difficulte()
-{
+function choix_difficulte() {
     let difficulte = new URLSearchParams(window.location.search).get("difficulte");
 
-    if (difficulte === "expert")
-    {
+    if (difficulte === "expert") {
         nb_mines = 99;
         largeur = 16;
         hauteur = 30;
     }
-    else if (difficulte === "intermediaire")
-    {
+    else if (difficulte === "intermediaire") {
         nb_mines = 40;
         largeur = 16;
         hauteur = 16;
     }
-    else
-    {
+    else {
         nb_mines = 10;
         largeur = 8;
         hauteur = 8;
@@ -35,33 +31,27 @@ function choix_difficulte()
 
 /// INITIALISATION ///
 
-function creation_matrices(premiere_generation)
-{
+function creation_matrices(premiere_generation) {
     matrice_mines = [];
     matrice_nombre_voisins = [];
     matrice_cases_cliques = [];
-    if (premiere_generation)
-    {
+    if (premiere_generation) {
         matrice_drapeaux = [];
     }
 
-    for (let x = 0; x < largeur; x++)
-    {
+    for (let x = 0; x < largeur; x++) {
         matrice_mines[x] = [];
         matrice_nombre_voisins[x] = [];
         matrice_cases_cliques[x] = [];
-        if (premiere_generation)
-        {
+        if (premiere_generation) {
             matrice_drapeaux[x] = [];
         }
-        
-        for (let y = 0; y < hauteur; y++)
-        {
+
+        for (let y = 0; y < hauteur; y++) {
             matrice_mines[x][y] = 0;
             matrice_nombre_voisins[x][y] = 0;
             matrice_cases_cliques[x][y] = 0;
-            if (premiere_generation)
-            {
+            if (premiere_generation) {
                 matrice_drapeaux[x][y] = 0;
             }
         }
@@ -72,34 +62,26 @@ function creation_matrices(premiere_generation)
     compte_voisins();
 }
 
-function dispose_mines()
-{
-    for (let i = 0; i < nb_mines; i++)
-    {
+function dispose_mines() {
+    for (let i = 0; i < nb_mines; i++) {
         let x = Math.floor(Math.random() * largeur);
         let y = Math.floor(Math.random() * hauteur);
 
-        if (matrice_mines[x][y] === 1){
+        if (matrice_mines[x][y] === 1) {
             i--;
-        }else {
+        } else {
             matrice_mines[x][y] = 1;
         }
     }
 }
 
-function compte_voisins()
-{
-    for (let x = 0; x < largeur; x++)
-    {
-        for (let y = 0; y < hauteur; y++)
-        {
-            for (let i = -1; i <= 1; i++)
-            {
-                for (let j = -1; j <= 1; j++)
-                {
-                    if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur)
-                    {
-                            matrice_nombre_voisins[x][y] += matrice_mines[x + i][y + j];
+function compte_voisins() {
+    for (let x = 0; x < largeur; x++) {
+        for (let y = 0; y < hauteur; y++) {
+            for (let i = -1; i <= 1; i++) {
+                for (let j = -1; j <= 1; j++) {
+                    if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur) {
+                        matrice_nombre_voisins[x][y] += matrice_mines[x + i][y + j];
                     }
                 }
             }
@@ -109,60 +91,46 @@ function compte_voisins()
 
 /// JEU ///
 
-function clic_case(x, y, type_clique)
-{
-    if (type_clique === "gauche")
-    {
-        if (matrice_drapeaux[x][y] === 1)
-        {
+function clic_case(x, y, type_clique) {
+    if (type_clique === "gauche") {
+        if (matrice_drapeaux[x][y] === 1) {
             alert("Vous ne pouvez pas cliquer sur une case avec un drapeau !")
         }
-        else
-        {
+        else {
             nombre_cliques++;
 
-            if (nombre_cliques === 1)
-            {
-                do
-                {
+            if (nombre_cliques === 1) {
+                do {
                     creation_matrices(false);
                 }
                 while (matrice_mines[x][y] === 1);
             }
 
-            if (matrice_cases_cliques[x][y] === 0)
-            {
+            if (matrice_cases_cliques[x][y] === 0) {
                 matrice_cases_cliques[x][y] = 1;
 
-                if (matrice_mines[x][y] === 1)
-                {
+                if (matrice_mines[x][y] === 1) {
                     partie_perdue();
                 }
-                else if (nombre_cases_non_minees_restantes() === 0)
-                {
+                else if (nombre_cases_non_minees_restantes() === 0) {
                     partie_remportee();
                 }
             }
         }
     }
-    else
-    {
+    else {
         place_drapeau(x, y);
     }
 
     affiche_matrices();
 }
 
-function nombre_cases_non_minees_restantes()
-{
+function nombre_cases_non_minees_restantes() {
     let nombre_cases_non_minees = 0;
 
-    for (let x = 0; x < largeur; x++)
-    {
-        for (let y = 0; y < hauteur; y++)
-        {
-            if (matrice_cases_cliques[x][y] === 0 && matrice_mines[x][y] === 0)
-            {
+    for (let x = 0; x < largeur; x++) {
+        for (let y = 0; y < hauteur; y++) {
+            if (matrice_cases_cliques[x][y] === 0 && matrice_mines[x][y] === 0) {
                 nombre_cases_non_minees++;
             }
         }
@@ -171,50 +139,38 @@ function nombre_cases_non_minees_restantes()
     return nombre_cases_non_minees;
 }
 
-function partie_remportee()
-{
-    window.location.href = "victoire.html";
+function partie_remportee() {
+    // window.location.href = "victoire.html";
 }
 
-function partie_perdue()
-{
-    window.location.href = "defaite.html";
+function partie_perdue() {
+    //  window.location.href = "defaite.html";
 }
 
-function place_drapeau(x, y)
-{
+function place_drapeau(x, y) {
 
-    if (matrice_drapeaux[x][y] === 1)
-    {
+    if (matrice_drapeaux[x][y] === 1) {
         matrice_drapeaux[x][y] = 0;
     }
-    else if (nombre_drapeaux_places() < nb_mines)
-    {
-        if (matrice_cases_cliques[x][y] === 0)
-        {
+    else if (nombre_drapeaux_places() < nb_mines) {
+        if (matrice_cases_cliques[x][y] === 0) {
             matrice_drapeaux[x][y] = 1;
         }
-        else
-        {
+        else {
             alert("Vous ne pouvez pas placer de drapeau sur une case déjà cliquée !");
         }
     }
-    else
-    {
+    else {
         alert("Nombre de drapeaux maximum atteint !");
     }
 }
 
-function nombre_drapeaux_places()
-{
+function nombre_drapeaux_places() {
     let nombre_drapeaux = 0;
 
-    for (let x = 0; x < largeur; x++)
-    {
-        for (let y = 0; y < hauteur; y++)
-        {
-            if (matrice_drapeaux[x][y] === 1)
-            {
+    for (let x = 0; x < largeur; x++) {
+        for (let y = 0; y < hauteur; y++) {
+            if (matrice_drapeaux[x][y] === 1) {
                 nombre_drapeaux++;
             }
         }
@@ -226,28 +182,20 @@ function nombre_drapeaux_places()
 /// IA ///
 
 
-function explorer()
-{
+function explorer() {
     let case_decouverte = false;
 
-    for (let x = 0; x < largeur; x++)
-    {
-        for (let y = 0; y < hauteur; y++)
-        {
-            if (matrice_cases_cliques[x][y] === 0)
-            {
-                for (let i = -1; i <= 1; i++)
-                {
-                    for (let j = -1; j <= 1; j++)
-                    {
+    for (let x = 0; x < largeur; x++) {
+        for (let y = 0; y < hauteur; y++) {
+            if (matrice_cases_cliques[x][y] === 0) {
+                for (let i = -1; i <= 1; i++) {
+                    for (let j = -1; j <= 1; j++) {
                         if (
                             case_decouverte === false
                             && (i !== 0 || j !== 0)
                             && x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur
-                        )
-                        {
-                            if (matrice_cases_cliques[x + i][y + j] === 1 && matrice_nombre_voisins[x + i][y + j] === 0)
-                            {
+                        ) {
+                            if (matrice_cases_cliques[x + i][y + j] === 1 && matrice_nombre_voisins[x + i][y + j] === 0) {
                                 case_decouverte = true;
 
                                 clic_case(x, y, "gauche");
@@ -259,51 +207,43 @@ function explorer()
         }
     }
 
-    if (case_decouverte === true)
-    {
+    if (case_decouverte === true) {
         explorer();
     }
 }
 
 /// AFFICHAGE ///
 
-function affiche_matrices()
-{
+function affiche_matrices() {
     let grille = document.getElementById("grille");
-    
-    while (grille.firstChild)
-    {
+
+    while (grille.firstChild) {
         grille.removeChild(grille.firstChild);
     }
 
-    for (let x = 0; x < largeur; x++)
-    {
+    for (let x = 0; x < largeur; x++) {
         let ligne = document.createElement("tr");
 
-        for (let y = 0; y < hauteur; y++)
-        {
+        for (let y = 0; y < hauteur; y++) {
             let case_ = document.createElement("td");
 
             case_.setAttribute("id", "case_" + x + "_" + y);
             case_.setAttribute("onclick", "clic_case(" + x + ", " + y + ", 'gauche')");
             case_.setAttribute("oncontextmenu", "clic_case(" + x + ", " + y + ", 'droite')");
 
-            if (matrice_cases_cliques[x][y] === 1)
-            {
+            if (matrice_cases_cliques[x][y] === 1) {
                 case_.setAttribute("class", "case case_cliquee");
-                
+
                 case_.innerHTML = matrice_nombre_voisins[x][y];
 
             }
-            else if (matrice_drapeaux[x][y] === 1)
-            {
+            else if (matrice_drapeaux[x][y] === 1) {
                 case_.setAttribute("class", "case case_drapeau");
             }
-            else
-            {
+            else {
                 case_.setAttribute("class", "case");
-            } if(matrice_mines[x][y] === 1){
-                case_.setAttribute("class","perdue")
+            } if (matrice_mines[x][y] === 1) {
+                case_.setAttribute("class", "perdue")
             }
 
             ligne.appendChild(case_);
@@ -324,24 +264,73 @@ function affiche_matrices()
 
 /// MAIN ///
 
-function main()
-{
+function main() {
     nombre_cliques = 0;
 
     choix_difficulte();
 
     creation_matrices(true);
-    
+
     affiche_matrices();
 
 }
+
+
+/** IA fonctionnel BFS */
+
+function iabfs() {
+    let max = largeur * hauteur; //Le nombre de case max pour les stats
+    let k = 0; //nb coups
+    let xdfs = 0;
+    let ydfs = 0;
+    let tabvoisin = [
+        [xdfs, ydfs],
+    ];
+    let tabvoisinclique = [];
+    while (nombre_cases_non_minees_restantes() !== 0 && (k < max)) {  //on s'arrête quand il n'y a plus de cases non minées (normalement, on a déja perdu ou gagné avant de sortir de la boucle)
+
+        clic_case(tabvoisin[0][0], tabvoisin[0][1], "gauche"); // clique sur le 1er element du tableau des voisins
+        tabvoisinclique.push([tabvoisin[0][0], tabvoisin[0][1]]);
+
+        tabvoisin = RemplirTabVoisin(tabvoisin, tabvoisinclique) //rajoute les cases voisines a la fin du tableau
+
+        tabvoisin.shift() // supprime le 1er element (celui qui vient d'être cliqué)
+
+        k++;
+    }
+}
+
+function RemplirTabVoisin(tabvoisin, tabvoisinclique) {
+    x = tabvoisin[0][0]
+    y = tabvoisin[0][1]
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur) {//on répartit aux cases adjacentes
+                if (!tabvoisin.some(element => element[0] === x + i && element[1] === y + j)) {
+                    if (!tabvoisinclique.some(element => element[0] === x + i && element[1] === y + j)) {
+                        if (i !== 0 || j !== 0) { // si i et j = 0 alors cela donne la même case que celle cliqué et on ne veut pas cela
+                            tabvoisin.push([x + i, y + j]);
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+    }
+    return tabvoisin
+}
+
+
+
 /** IA fonctionnel à benchmark */
 function ia11() {
-    let max = largeur*hauteur; //Le nombre de case max pour les stats
-    let k =0; //nb coups
+    let max = largeur * hauteur; //Le nombre de case max pour les stats
+    let k = 0; //nb coups
     let int8 = Array(largeur).fill([]).map((x) => x = Array(hauteur).fill(255)); //création d'une matrice contenant des valeurs inatteignables
-    while (nombre_cases_non_minees_restantes() !== 0 &&  (k<max)){  //on s'arrête quand il n'y a plus de cases non minées (normalement, on a déja perdu ou gagné avant de sortir de la boucle)
-        let min = [Number.MAX_SAFE_INTEGER,0,0]; //on met la plus grande valeur possible pour être sur (mais bon, c'est une valeur arbitraire)
+    while (nombre_cases_non_minees_restantes() !== 0 && (k < max)) {  //on s'arrête quand il n'y a plus de cases non minées (normalement, on a déja perdu ou gagné avant de sortir de la boucle)
+        let min = [Number.MAX_SAFE_INTEGER, 0, 0]; //on met la plus grande valeur possible pour être sur (mais bon, c'est une valeur arbitraire)
         for (let x = 0; x < largeur; x++) { //on parcourt chaque case de la grille
             for (let y = 0; y < hauteur; y++) {
                 let element = "case_" + x + "_" + y;  //je dois modifier ça pour que l'algo tourne sans l'interface graphique
@@ -354,9 +343,9 @@ function ia11() {
         distribuXtoVoisin(int8); //on répartit les valeurs
         compteNan(int8); //on discrimine les potentielles mines
         distribuXtoVoisin(int8); //on recalcule en prenant en compte les potentielles mines
-        min = choice(int8,min); //on choisit le cas la plus safe
+        min = choice(int8, min); //on choisit le cas la plus safe
         clic_case(min[1], min[2], "gauche") //on clique dessus
-        console.log(k +":"+ max +":"+nombre_cases_non_minees_restantes());
+        console.log(k + ":" + max + ":" + nombre_cases_non_minees_restantes());
         explorer(); //s'il y a des cases avec comme valeur 0.
         k++;
     }
@@ -367,9 +356,9 @@ function ia11() {
  * à des cases non explorées (et donc potentiellement à une mine).
  * Plus cette somme est élevé, moins on a de chance de cliquer dessus.
  * */
-function distribuXtoVoisin(tableau){
-    for (let x = 0; x < largeur; x++){//on parcourt
-        for (let y = 0; y < hauteur; y++){
+function distribuXtoVoisin(tableau) {
+    for (let x = 0; x < largeur; x++) {//on parcourt
+        for (let y = 0; y < hauteur; y++) {
             let element = "case_" + x + "_" + y; //je dois modifier ça pour que l'algo tourne sans l'interface graphique
             let value = document.getElementById(element).textContent;
             if (value !== "" || value != 0) { //répartir une case non explorée, c'est inutile et les cases valant 0 ne nous donne aucune info sur des mines
@@ -377,8 +366,10 @@ function distribuXtoVoisin(tableau){
                 for (let i = -1; i <= 1; i++) {
                     for (let j = -1; j <= 1; j++) {
                         if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur) {//on répartit aux cases adjacentes
-                            tableau[x+i][y+j] %= 255; //on lui donne une chance d'être explore
-                            if(tableau[x+i][y+j] != -1)tableau[x+i][y+j] += value; //on ne veut pas que les potentielles mines soient selectionnées (je vais modifié ça aussi)
+                            tableau[x + i][y + j] %= 255; //on lui donne une chance d'être explore
+                            console.log("tabbvoisin : " + tableau)
+                            if (tableau[x + i][y + j] != -1) tableau[x + i][y + j] += value;
+                            console.log("tabvoisin mine :" + tableau) //on ne veut pas que les potentielles mines soient selectionnées (je vais modifié ça aussi)
                         }
                     }
                 }
@@ -389,7 +380,7 @@ function distribuXtoVoisin(tableau){
 /**
  * on choisit la case la plus safe d'après nos calculs
  * */
-function choice(int8,min){
+function choice(int8, min) {
     for (let x = 0; x < largeur; x++) {//on parcourt
         for (let y = 0; y < hauteur; y++) {
             if (int8[x][y] < min[0] && int8[x][y] > 0) { //on veut la plus petite valeur càd, celle qui a le moins de chance d'être une mine
@@ -402,28 +393,22 @@ function choice(int8,min){
     return min;
 }
 
-function compteNan(tableau)
-{
-    for (let x = 0; x < largeur; x++)
-    {
-        for (let y = 0; y < hauteur; y++)
-        {
-            if(tableau[x][y] !== 255 && !isNaN(tableau[x][y])){ //on enlève les cases non explorées
-                let k=0;
-                for (let i = -1; i <= 1; i++)
-                {
-                    for (let j = -1; j <= 1; j++)
-                    {
-                        if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur)
-                        {
-                            k+=(isNaN(tableau[x+i][y+j])); //on compte le nombre de case adjacente qui ont été exploré
+function compteNan(tableau) {
+    for (let x = 0; x < largeur; x++) {
+        for (let y = 0; y < hauteur; y++) {
+            if (tableau[x][y] !== 255 && !isNaN(tableau[x][y])) { //on enlève les cases non explorées
+                let k = 0;
+                for (let i = -1; i <= 1; i++) {
+                    for (let j = -1; j <= 1; j++) {
+                        if (x + i >= 0 && x + i < largeur && y + j >= 0 && y + j < hauteur) {
+                            k += (isNaN(tableau[x + i][y + j])); //on compte le nombre de case adjacente qui ont été exploré
                         }
                     }
                 }
-                if((tableau[x][y] /= k) >=1 && k>=3){ //c'est potentiellement une mine
+                if ((tableau[x][y] /= k) >= 1 && k >= 3) { //c'est potentiellement une mine
                     tableau[x][y] = -1; //(je vais aussi modifier ça avec une liste)
                 }
-                
+
             }
         }
     }
